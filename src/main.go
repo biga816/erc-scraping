b4fe7ebcfee44bf9ac44c3fca6fba006868224b2
akihiro.tanaka <big.a.0816@gmail.com>
2020-01-26 23:26:16 +0900

    update: add ERCInfo struct

--- src/main.go
+++ src/main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"reflect"
 	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+// ERCInfo erc info
+type ERCInfo struct {
+	Eip      string
+	Title    string
+	Category string
+	Status   string
+	URL      string
+	Type     string
+	Created  string
+}
 
 func main() {
 	url := "https://github.com/ethereum/EIPs/tree/master/EIPS"
@@ -20,7 +34,6 @@ func main() {
 
 		r := regexp.MustCompile(`^eip-.*\.md$`)
 		if r.MatchString(text) && hasHref {
-			println("====https://github.com" + href + "====")
 			scrape("https://github.com" + href)
 		}
 	})
@@ -42,13 +55,23 @@ func scrape(url string) {
 		})
 
 		// get data
+		ercInfo := ERCInfo{}
+		p := reflect.ValueOf(&ercInfo).Elem()
 		table.Find("tbody").Find("td").Each(func(idx int, s *goquery.Selection) {
 			if len(titleList) > idx {
 				title := titleList[idx]
-				if title == "eip" || title == "title" || title == "category" || title == "status" {
-					println("-> " + title + ": " + s.Text())
+				if title == "eip" || title == "title" || title == "category" || title == "status" || title == "type" || title == "created" {
+					// println("-> " + title + ": " + s.Text())
+					fieldName := strings.ToUpper(title[0:1]) + title[1:len(title)]
+					p.FieldByName(fieldName).SetString(s.Text())
 				}
 			}
 		})
+
+		// check if the EIP is ERC
+		if strings.ToLower(ercInfo.Category) == "erc" && strings.ToLower(ercInfo.Status) == "final" {
+			ercInfo.URL = url
+			fmt.Printf("%+v\n", ercInfo)
+		}
 	}
 }
